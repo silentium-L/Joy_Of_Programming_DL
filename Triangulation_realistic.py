58471768cb7fb4434549e6bd04b75bf7c455ecfe
silentium-L <lorenscheit.dennis.99@gmail.com>
2025-07-24 17:55:12 +0200

    change

--- Triangulation_realistic.py
+++ Triangulation_realistic.py
@@ -7,10 +7,25 @@ SimEnv.connect()
 env = SimEnvManager.first()
 
 while SimEnv.run_main():
+
+    #Defining main Objects of use for this Scope
+    platform  = MovablePlatform.find("platform")
+
     # main loop to retrieve data from the SimEnv, calculate stuff and send commands back into the SimEnv
     # for example, get current time and display it
     simtime = env.get_sim_time()
     print(f"current time: {simtime} seconds")
+
+    #Move Platform to the correct Space
+    platform.setTargetLocation(6,0,0)
+    platform.setTargetLocation(6,5,0)
+
+    #Function to grab the Barrel from that Point
+    getBarrel()
+
+    #Move Platform back
+    set_target_location(6,0,0)
+    set_target_location(0,0,0)
 
 	
 #cleanup close code
